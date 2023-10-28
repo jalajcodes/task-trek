197f5bad0b26ec969506c771b316a5c1b231ce5f
jalajcodes <jalaj@mail.com>
2023-10-28 18:18:07 +0530

    chore: deploy changes

--- server/app.js
+++ server/app.js
@@ -1,17 +1,24 @@
-const express = require('express');
-require('express-async-errors');
-const cors = require('cors');
-const middleware = require('./utils/middleware');
-const authRoutes = require('./routes/auth');
-const entryRoutes = require('./routes/entry');
+const express = require("express");
+require("express-async-errors");
+const cors = require("cors");
+const middleware = require("./utils/middleware");
+const authRoutes = require("./routes/auth");
+const entryRoutes = require("./routes/entry");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', authRoutes);
-app.use('/api/entries', entryRoutes);
+app.use(express.static("build"));
+
+app.use("/api", authRoutes);
+app.use("/api/entries", entryRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build/index.html"));
+});
 
 app.use(middleware.unknownEndpointHandler);
 app.use(middleware.errorHandler);
